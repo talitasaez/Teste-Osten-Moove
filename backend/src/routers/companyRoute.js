fbe41d04d18529e8bdebd38198bd377547413e93
talitasaez <tltsaez@gmail.com>
2023-05-22 11:02:47 -0300

    criacao da pasta routers e arquivo route.js inclui logica das rotas

---
+++ backend/src/routers/companyRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const companyController = require('../controllers/companyController');
+
+router.get('/', companyController.getAllCompanies);
+router.post('/', companyController.createCompany);
+router.get('/:id', companyController.getCompanyById);
+router.put('/:id', companyController.updateCompany);
+router.delete('/:id', companyController.deleteCompany);
+
+module.exports = router;
